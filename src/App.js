d8eefd109942d61dbced01c9291e674172891f2a
Kashish <kashishverma8382@gmail.com>
2023-05-19 18:59:39 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [employees, setEmployees] = useState([]);
+  const [filteredEmployees, setFilteredEmployees] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/dixitsoham7/dixitsoham7.github.io/main/index.json')
+      .then((response) => response.json())
+      .then((data) => {
+        setEmployees(data.employees);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (!filter) {
+      setFilteredEmployees(employees);
+    } else {
+      const filtered = employees.filter((employee) => {
+        const name = employee.name ? employee.name.toLowerCase() : '';
+        const designation = employee.designation ? employee.designation.toLowerCase() : '';
+        const skills = employee.skills ? employee.skills.map((skill) => skill.toLowerCase()) : [];
+
+        return (
+          name.includes(filter.toLowerCase()) ||
+          designation.includes(filter.toLowerCase()) ||
+          skills.some((skill) => skill.includes(filter.toLowerCase()))
+        );
+      });
+      setFilteredEmployees(filtered);
+    }
+  }, [employees, filter]);
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Employee Filter</h1>
+      <input type="text" value={filter} onChange={handleFilterChange} placeholder="Filter by name/designation/skills" />
+      <ul>
+        {filteredEmployees.map((employee) => (
+          <li key={employee.id}>
+            <h2>{employee.name}</h2>
+            <p>Designation: {employee.designation}</p>
+            <p>Skills: {employee.skills.join(', ')}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
